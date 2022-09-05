e8e01bb247e33cc7b40b302745d58e1f0b5ef5cb
shohel-ranaC <akashshohel422@gmail.com>
2022-09-05 08:36:32 +0600

    increment and decrement concept

---
+++ incrementDecrement.js
@@ -0,0 +1,27 @@
+let a = 10;
+a--;
+a++;
+--a;
+a--;
+a++;
+console.log(++a, ++a, a--);
+
+a = 5
+++a;          // a becomes 6
+a++;          // a becomes 7
+--a;          // a becomes 6
+a--;          // a becomes 5
+
+let y = 5;
+increment = y++, y++, --y, --y, y--, y++;
+console.log(increment);
+
+let z = 20;
+count = --z, z--, --z, ++z, z--;
+console.log(count);
+
+// var num = 10;
+// for (var i = 0; i < num; i++) {
+//     console.log(i, num);
+
+// }
